8a9178c799740c156cf87acfcaf9d78181b4e3ea
Nick Cho <nicholas.cho@hotmail.ca>
2024-02-10 06:45:11 -0500

    Configuring cognito pool IAC

--- services/authService/.terraform/main.tf
+++ services/authService/.terraform/main.tf
@@ -1,88 +1,80 @@
 resource "aws_cognito_user_pool" "userpool" {
-    name = "opengym-userpool"
+  name = "opengym-userpool"
 
-        
-    schema {
-        attribute_data_type = "String"
-        name = "email"
-        required = true
-        developer_only_attribute = false
-        mutable = true
+  schema {
+    attribute_data_type      = "String"
+    name                     = "email"
+    required                 = true
+    developer_only_attribute = false
+    mutable                  = true
+  }
+
+  email_configuration {
+    email_sending_account = "COGNITO_DEFAULT"
+  }
+
+  auto_verified_attributes = ["email"]
+
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = true
+    require_uppercase = true
+  }
+
+  username_attributes = ["email"]
+  username_configuration {
+    case_sensitive = true
+  }
+
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
     }
-
-    email_configuration {
-        email_sending_account = "COGNITO_DEFAULT"
-    }
-
-    auto_verified_attributes = ["email"]
-
-    password_policy {
-        minimum_length = 8
-        require_lowercase = true
-        require_numbers = true
-        require_symbols = true
-        require_uppercase = true
-    }
-
-    username_attributes = ["email"]
-    username_configuration {
-        case_sensitive = true
-    }
-
-    account_recovery_setting {
-        recovery_mechanism {
-            name = "verified_email"
-            priority = 1
-        }
-    }
-
-    # mfa_configuration          = "ON"
-    # sms_authentication_message = "Your code is {####}"
-
-    # sms_configuration {
-    #     external_id    = "example"
-    #     sns_caller_arn = aws_iam_role.example.arn
-    #     sns_region     = "us-east-1"
-    # }
-
-    # software_token_mfa_configuration {
-    #     enabled = true
-    # }
+  }
 }
 
-resource "aws_cognito_user_pool client" "userpool_client" {
-    name = "opengym-userpool-client"
-    user_pool_id = aws_cognito_user_pool.userpool.id
-    explicit_auth_flows = [ "ALLOW_SRP_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_PASSWORD_AUTH" ]
-    generate_secret = false
-    prevent_user_existence_errors = "LEGACY"
-    refresh_token_validity = 1
-    access_token_validity = 1
-    id_token_validity = 1
-    token_validity_units = {
-        access_token = "hours"
-        id_token = "hours"
-        refresh_token = "hours"
-    }
+resource "aws_cognito_user_pool_client" "userpool_client" {
+  name                                 = "opengym-userpool-client"
+  user_pool_id                         = aws_cognito_user_pool.userpool.id
+  explicit_auth_flows                  = ["ALLOW_SRP_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_PASSWORD_AUTH"]
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_scopes                 = ["email", "openid", "phone"]
+  # To be configured later to get user back into site
+  # callback_urls = [] 
+  # default_redirect_uri = 
+  generate_secret               = false
+  prevent_user_existence_errors = "LEGACY"
+  refresh_token_validity        = 1
+  access_token_validity         = 1
+  id_token_validity             = 1
+  token_validity_units {
+    access_token  = "hours"
+    id_token      = "hours"
+    refresh_token = "hours"
+  }
 }
 
-resource "aws_api_gateway_rest_api" "auth_api" {
-    name = "opengym-auth-api"
-    description = "This is the OpenGym Auth API to handle user authentication and getting user information"
-}
+# resource "aws_api_gateway_rest_api" "auth_api" {
+#     name = "opengym-auth-api"
+#     description = "This is the OpenGym Auth API to handle user authentication and getting user information"
+# }
 
-resource "aws_api_gateway_resource" "auth_resource" {
-    rest_api_id = aws_api_gateway_rest_api.api.id
-    parent_id = aws_api_gateway_rest_api.api.root_resource_id
-    path_part = "auth"
-}
+# resource "aws_api_gateway_resource" "auth_resource" {
+#     rest_api_id = aws_api_gateway_rest_api.api.id
+#     parent_id = aws_api_gateway_rest_api.api.root_resource_id
+#     path_part = "auth"
+# }
 
-resource "aws_api_gateway_method" "auth_gw_method" {
-    rest_api_id = aws_api_gateway_rest_api.api.id
-    resource_id = aws_api_gateway_resource.auth_resource.id
-    http_method = "GET"
-    authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "auth_gw_method" {
+#     rest_api_id = aws_api_gateway_rest_api.api.id
+#     resource_id = aws_api_gateway_resource.auth_resource.id
+#     http_method = "GET"
+#     authorization = "NONE"
+# }
 
 # For integration with lambda function
 # resource "aws_api_gateway_integration" "auth_gw_integration" {
