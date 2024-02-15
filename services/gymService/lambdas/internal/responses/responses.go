16325424251b1982d1e2d3f81e25afbe01b9d770
Nick Cho <nicholas.cho@hotmail.ca>
2024-02-15 09:38:54 -0500

    Creating make response package

---
+++ services/gymService/lambdas/internal/responses/responses.go
@@ -0,0 +1,27 @@
+package responses
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func CreateMessageResponse(statusCode int, resMsg string) events.APIGatewayProxyResponse {
+	// StatusOk = 200
+	// StatusCreated = 201
+	// StatusAccepted = 202
+	// StatusNoContent = 204
+	// StatusBadRequest = 400
+	// StatusUnauthorized = 401
+	// StatusPayMentRequired = 402
+	// StatusForbidden = 403
+
+	response := events.APIGatewayProxyResponse{
+		StatusCode: statusCode,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin":      "*",
+			"Access-Control-Allow-Headers":     "*",
+			"Access-Control-Allow-Credentials": "true",
+		},
+		Body: resMsg,
+	}
+	return response
+}
