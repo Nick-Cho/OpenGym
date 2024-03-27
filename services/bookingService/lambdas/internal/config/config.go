43706b01a9483deac25ff881abb35c7121fe7723
Nick Cho <nicholas.cho@hotmail.ca>
2024-03-27 08:28:50 -0400

    Setting up lambdas booking service environment

---
+++ services/bookingService/lambdas/internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	db  *sql.DB
+	err error
+)
+
+func Connect() *sql.DB {
+	// err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error getting env in config.go, not coming through %v \n", err)
+	}
+
+	dbDriver := "mysql"
+	dbUser := "gym_admin"
+	dbPass := os.Getenv("DB_PASS")
+	dbName := "booking_db"
+	dbIp := os.Getenv("DB_IP")
+
+	db, err = sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbIp+")/"+dbName)
+
+	if err != nil {
+		log.Printf("Error %s when connecting to DB\n", err)
+	}
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(20)
+	db.SetConnMaxLifetime(time.Minute * 5)
+
+	return db
+}
