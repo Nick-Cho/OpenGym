fee051517b9c7d36dac8c924f62c0aba7577a7f6
Nick Cho <nicholas.cho@hotmail.ca>
2024-03-29 21:14:00 -0400

    implementing get bookigs lamda functionality

--- services/bookingService/lambdas/get_bookings/handler.go
+++ services/bookingService/lambdas/get_bookings/handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 
 	config "bookingService/internal/config"
@@ -14,6 +15,17 @@ import (
 
 type Handler struct{}
 
+type TimeSlotsInfo struct {
+	SlotId         int    `json:"SlotId"`
+	GymId          int    `json:"GymId"`
+	StartTime      string `json:"StartTime"`
+	EndTime        string `json:"EndTime"`
+	Date           string `json:"Date"`
+	MaxSlots       int    `json:"MaxSlots"`
+	AvailableSlots int    `json:"AvailableSlots"`
+	Version        int    `json:"Version"`
+}
+
 func (h *Handler) HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var requestBody map[string]any
 	if request.Body == "" {
@@ -24,4 +36,39 @@ func (h *Handler) HandleRequest(ctx context.Context, request events.APIGatewayPr
 
 	db := config.Connect()
 	json.Unmarshal([]byte(request.Body), &requestBody)
+
+	date := requestBody["date"]
+	gym_id := requestBody["gym_id"]
+	start_time := requestBody["start_time"]
+	end_time := requestBody["end_time"]
+
+	f_gym_id := gym_id.(float64)
+	gym_id = int(f_gym_id)
+
+	sqlReq := fmt.Sprintf("SELECT * FROM TimeSlots WHERE GymId='%d' AND Date='%s' AND StartTime='%s' AND EndTime='%s'", gym_id, date, start_time, end_time)
+	res, err := db.Query(sqlReq)
+
+	if err != nil {
+		log.Println("Error querying booking info: ", err)
+		return response.CreateMsgResp(400, fmt.Sprintf("Error querying booking info: %s", err)), nil
+	}
+
+	var timeSlots TimeSlotsInfo
+	res.next()
+
+	err = res.Scan(&timeSlots.SlotId, &timeSlots.GymId, &timeSlots.StartTime, &timeSlots.EndTime, &timeSlots.Date, &timeSlots.MaxSlots, &timeSlots.AvailableSlots, &timeSlots.Version)
+
+	if err != nil {
+		log.Println("Error scanning booking info: ", err)
+		return response.CreateMsgResp(400, fmt.Sprintf("Error scanning booking info: %s", err)), nil
+	}
+
+	res, err = json.Marshal(timeSlots)
+	if err != nil {
+		log.Println("ERROR MARSHALLING RESPONSE BODY TO JSON", err)
+		return response.CreateMsgResp(400, fmt.Sprintf("Error marshalling response body to JSON: %s", err)), nil
+	}
+
+	return response.CreateMsgResp(200, string(res)), nil
+
 }
